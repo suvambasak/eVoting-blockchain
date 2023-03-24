1877dc686849e6d2dbc25448ee6b1034df6f147d
Suvam Basak <suvambasak2008@gmail.com>
2023-03-24 12:49:19 +0530

    Handling POST request

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+## Start App
+```bash
+export FLASK_APP=flask_app
+```
+```bash
+export FLASK_DEBUG=1 
+```
+```bash
+flask run
+```
--- backend/flask_app/auth.py
+++ backend/flask_app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request, redirect
 
 auth = Blueprint('auth', __name__)
 
@@ -11,3 +11,24 @@ def index():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+@auth.route('/signin', methods=['POST'])
+def signin_post():
+    roll_number = request.form.get('rollno')
+    password = request.form.get('pwd')
+
+    print(roll_number, password)
+
+    return redirect(url_for('main.candidates'))
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    roll_number = request.form.get('rollno')
+    wallet_address = request.form.get('walletaddr')
+    password = request.form.get('pwd')
+
+    print(roll_number, wallet_address, password)
+
+    return redirect(url_for('auth.index'))
