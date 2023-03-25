4d334fca7f1ef598fa75c98fe32f591c94633395
Suvam Basak <suvambasak2008@gmail.com>
2023-03-25 11:35:05 +0530

    Show candidate list after login

--- backend/flask_app/main.py
+++ backend/flask_app/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, url_for
 from flask_login import login_required, current_user
+from .models import Candidate
 
 
 main = Blueprint('main', __name__)
@@ -8,4 +9,9 @@ main = Blueprint('main', __name__)
 @main.route('/candidates')
 @login_required
 def candidates():
-    return render_template('candidates.html', user=current_user)
+    candidates = Candidate.query.with_entities(
+        Candidate.id,
+        Candidate.roll_number,
+        Candidate.name
+    ).all()
+    return render_template('candidates.html', user=current_user, candidates=candidates)
