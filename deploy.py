a131779c5a941ccbeb301a1d3efd57fc84dd1eba
Suvam Basak <suvambasak2008@gmail.com>
2023-04-12 23:16:56 +0530

    New deployment address update in admin details

--- deploy.py
+++ deploy.py
@@ -12,18 +12,20 @@ solcx.install_solc('0.8.19')
 w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URL))
 sepolia = 11155111
 
-
+# Input private key
 private_key = getpass.getpass(prompt='Private key: ')
 private_key = '0x'+private_key
 
 sys.stdout.write(f'\r Reading admin details...          ')
-with open('admin/admin.json')as json_file:
+with open('admin/admin.json') as json_file:
     admin_details = json.loads(json_file.read())
     admin_wallet = admin_details['wallet']
 sys.stdout.write(f'\r Reading contract...               ')
 with open('contract/mysol2.sol', 'r') as contract_file:
     contract = contract_file.read()
 
+
+# Compile
 sys.stdout.write(f'\r Compiling contract...               ')
 compiled_solidity = compile_standard(
     {
@@ -43,6 +45,8 @@ compiled_solidity = compile_standard(
 )
 ABI = compiled_solidity['contracts']['evote.sol']['EVoting']['abi']
 BYTECODE = compiled_solidity['contracts']['evote.sol']['EVoting']['evm']['bytecode']['object']
+
+# Deployment
 contract_instance = w3.eth.contract(abi=ABI, bytecode=BYTECODE)
 nonce = w3.eth.getTransactionCount(admin_wallet)
 
@@ -62,7 +66,13 @@ sys.stdout.write(f'\r Waiting for Tx receipt...             ')
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
 
+admin_details['contract_address'] = tx_receipt.contractAddress
 print(f'\n contract_address: {tx_receipt.contractAddress}')
 
+
+# Writing ABI and contract address
 with open('contract/ABI.json', 'w') as ABI_file:
     ABI_file.write(json.dumps(ABI))
+
+with open('admin/admin.json', 'w') as json_file:
+    json_file.write(json.dumps(admin_details, indent=4))
